c5b14bfcae9d1aa51303554e7c32abc67a03f908
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-08-31 17:11:42 -0400

    update example

--- R/test.R
+++ R/test.R
@@ -4,7 +4,21 @@
 #'
 #' @return A message
 #'
+#' @inheritParams testing
+#'
 #' @export
-test <- function() {
+test <- function(b) {
     message("This is a test")
+}
+
+
+#' Another test function
+#'
+#' @param a,b character(1) A letter, either lowercase or uppercase
+#'
+#' @return Another message
+#'
+#' @export
+testing <- function(a, b) {
+    message("This is also a test")
 }
