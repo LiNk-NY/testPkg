06aca5e908bb913c50bd338d57d308dd7f839380
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-08-31 17:28:18 -0400

    remove extra files

--- inst/scripts/figure1.R
+++
@@ -1,9 +0,0 @@
-data(mtcars)
-library(psych)
-
-nummtcars <- mtcars[, -which(names(mtcars) %in% c("vs", "am"))]
-
-pdf("vignettes/mtcarpairs.pdf")
-pairs.panels(nummtcars, hist.col = "darkgreen", cex.cor = 1.8)
-dev.off()
-
